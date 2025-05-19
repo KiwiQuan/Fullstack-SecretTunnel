6ba2f40913928ccd273e972771e491c7c5d8cc0b
ZeeQ <kw133875@gmail.com>
2025-05-19 12:58:46 -0500

    asd

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-
+import axios from "axios";
 const API = "https://fsa-jwt-practice.herokuapp.com";
 
 const AuthContext = createContext();
@@ -9,10 +9,42 @@ export function AuthProvider({ children }) {
   const [location, setLocation] = useState("GATE");
 
   // TODO: signup
+  const signup = async (username) => {
+    try {
+      const response = await axios.post(`${API}/signup`, {
+        username,
+        password: "secret",
+      });
 
+      if (response.data.success) {
+        setToken(response.data.token);
+        setLocation("TABLET");
+      } else {
+        throw new Error("signup failed");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   // TODO: authenticate
-
-  const value = { location };
+  const authenticate = async () => {
+    if (!token) {
+      throw new Error("No token found");
+    }
+    try {
+      const result = await axios(`${API}/authenticate`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (result.data.success) {
+        setLocation("TUNNEL");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const value = { location, signup, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
